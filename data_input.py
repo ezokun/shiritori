aa696d426593473a3f682c77d155a0e36c4513f2
ezokun <ezokun@gmail.com>
2018-11-11 07:32:15 +0900

    Add files via upload

---
+++ data_input.py
@@ -0,0 +1,115 @@
+import sqlite3
+import csv
+
+# 変換用リスト
+# 実装方式が違うのでリストの持ち方も違う
+# 上のほうが早いが面倒だったのでとりあえずの実装
+large_list = {'ァ': 'ア', 'ィ': 'イ', 'ゥ': 'ウ', 'ェ': 'エ', 'ォ': 'オ',
+              'ッ': 'ツ', 'ャ': 'ヤ', 'ュ': 'ユ', 'ョ': 'ヨ', 'ヮ': 'ワ'}
+clean_list = ['ヴガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヷヸヹヺ',
+              'ウカキクケコサシスセソタチツテトハヒフヘホハヒフヘホワヰヱヲ']
+long_list = ['アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰウヱヲンヴガギグゲゴザジズゼゾダヂヅデドバビブベボヷヸヹヺパピプペポァィゥェォッャュョヮー',
+             'アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアウオアイウエオアイウエオンウアイウエオアイウエオアイウエオアイウエオアイエオアイウエオアイウエオウアウオアー']
+
+def enlarge(convert_word):
+    """
+    小書き文字変換
+    """
+    if convert_word in large_list.keys():
+        return large_list[convert_word]
+    return convert_word
+
+def cleaning(convert_word):
+    """
+    濁音・半濁音変換
+    """
+    # 走査を2回かけるので相対的には遅い
+    if convert_word in clean_list[0]:
+        return clean_list[1][clean_list[0].index(convert_word)]
+    return convert_word
+
+def change_long(convert_word):
+    """
+    長音をア行に変換
+    """
+    return long_list[1][long_list[0].index(convert_word)]
+
+
+# DBファイル名
+dbname = 'word_neologd.sqlite3'
+# DB接続・カーソル取得
+conn = sqlite3.connect(dbname)
+c = conn.cursor()
+
+# 挿入用SQL
+insert_sql = 'insert into ' + \
+                'word (name, reading, count, ' + \
+                'first, last, first_clean, last_large, last_clean, ' + \
+                'last_large_long, last_clean_long, cost) ' + \
+                'values (?,?,?,?,?,?,?,?,?,?,?)'
+
+file_list = ['./data/Noun.adjv.csv',
+             './data/Noun.adverbal.csv',
+             './data/Noun.csv',
+             './data/Noun.verbal.csv',
+             './data/Noun.demonst.csv',
+             './data/Noun.nai.csv',
+             './data/Noun.name.csv',
+             './data/Noun.number.csv',
+             './data/Noun.org.csv',
+             './data/Noun.others.csv',
+             './data/Noun.place.csv',
+             './data/Noun.proper.csv']
+# file_list = ['./data/mecab-user-dict-seed.20180920.csv']
+
+for one_file in file_list:
+    # ファイルオープン
+    print(one_file)
+    with open(one_file, 'r', encoding='euc-jp') as csvfile:
+    # with open(one_file, 'r', encoding='utf8') as csvfile:
+        # csvファイル読み込み
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        # 行取得
+        for row in csv_reader:
+            # とりあえず名詞だけ取得
+            if row[4] != '名詞':
+                continue
+
+            # # ンは登録しない!
+            # if row[11][-1] == 'ン':
+            #     continue
+
+            # 先頭文字を清音に
+            first_clean = cleaning(enlarge(row[11][0]))
+
+            # 末尾の長音をはずす
+            last_word = row[11][-1] if row[11][-1] != 'ー' else row[11][-2]
+
+            # 末尾の小書き文字を変換
+            # 拗音を1文字とするルールには未対応
+            #   その場合文字数はどうなるんだろ
+            last_large = enlarge(last_word)
+
+            # 末尾文字を清音に
+            # 拗音対応する場合は2カラム追加が必要
+            last_clean = cleaning(last_large)
+
+            # 末尾の長音を変換する
+            last_large_long = last_large if row[11][-1] != 'ー' else change_long(row[11][-2])
+            last_clean_long = last_clean if row[11][-1] != 'ー' else change_long(row[11][-2])
+
+            # DB登録
+            word = (row[0], row[11], len(row[11]),
+                    row[11][0], row[11][-1],
+                    first_clean, last_large, last_clean,
+                    last_large_long, last_clean_long, int(row[3]))
+
+            # 重複エラーは除く
+            try:
+                c.execute(insert_sql, word)
+            except sqlite3.IntegrityError:
+                pass
+                #print("重複")
+
+conn.commit()
+conn.close()
---
+++ shiritori.php
@@ -0,0 +1,121 @@
+<?php
+$return_array = [];
+// 処理種別で切り分け
+if (!isset($_POST['type'])) {
+    $return_array['error'] = '*no type error';
+}
+else {
+    // 単語の存在チェック
+    if (strcmp($_POST['type'], 'ask') === 0) {
+        if (!isset($_POST['word']) or !isset($_POST['target'])) {
+            $return_array['error'] = '*no word error';
+        }
+        else {
+            $return_array = check_word($_POST['word'], $_POST['target']);
+        }
+    }
+    // 解答取得
+    elseif (strcmp($_POST['type'], 'ans') === 0) {
+        if (!isset($_POST['first']) or !isset($_POST['count'])) {
+            $return_array['error'] = '*no param error';
+        }
+        else {
+            $return_array = get_word($_POST['first'], $_POST['count']);
+        }
+    }
+}
+// 返信
+header('Content-Type: text/javascript; charset=utf-8');
+echo(json_encode($return_array));
+
+
+// 対象の単語から読みを取得する
+function check_word($word, $target)
+{
+    $return_array = NULL;
+    try {
+        // データ取得
+        $sql = 'SELECT name, reading, first_clean, last_clean_long '.
+                'FROM word WHERE name = ? order by cost';
+        $pdo = new PDO('sqlite:word_neologd.sqlite3');
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$word]);
+        $results = $stmt->fetchAll();
+    }
+    catch (Exception $e) {
+    }
+
+    // 結果なし
+    if (count($results) === 0) {
+        $return_array['message'] = '※その言葉は知りません。: ' . 
+                htmlspecialchars($_POST['word'], ENT_QUOTES);
+        $return_array['word'] = htmlspecialchars($_POST['word'], ENT_QUOTES);
+        return $return_array;
+    }
+
+    // 読みを検索し、対象の文字から始まる単語を取得
+    $last_nn = FALSE;
+    foreach ($results as $result) {
+        if (strcmp($target, $result['first_clean']) === 0) {
+            // 「ん」で終わる読み方の場合
+            if (strcmp('ン', $result['last_clean_long']) === 0) {
+                $last_nn = TRUE;
+                $return_array['reading'] = $result['reading'];
+            }
+            else {
+                $last_nn = FALSE;
+                $return_array['name'] = htmlspecialchars($result['name'], ENT_QUOTES);
+                $return_array['reading'] = $result['reading'];
+                $return_array['last'] = $result['last_clean_long'];
+                break;
+            }
+        }
+    }
+
+    if ($last_nn) {
+        $return_array['message'] = '※「' . 
+                htmlspecialchars($_POST['word'], ENT_QUOTES) .
+                '(' . $return_array['reading'] . ')' .
+                '」の読みは「ン」で終わります。';
+        $return_array['word'] = htmlspecialchars($_POST['word'], ENT_QUOTES);
+    }
+    elseif ($return_array === NULL) {
+        $return_array['message'] = '※「' . $target .
+                '」で始まる読み方を知りません。: ' . 
+                htmlspecialchars($_POST['word'], ENT_QUOTES);
+        $return_array['word'] = htmlspecialchars($_POST['word'], ENT_QUOTES);
+    }
+    return $return_array;
+}
+
+// 指定した文字で始まる単語を、
+// 取得回数に応じて取得する
+function get_word($first, $count) {
+    $return_array = NULL;
+    try {
+        // データ取得
+        $sql = 'SELECT name, reading, first_clean, last_clean_long '. 
+                'FROM word WHERE first_clean = ? ' .
+                'order by cost limit 1 offset ?';
+        $pdo = new PDO('sqlite:word_ipadic.sqlite3');
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$first, $count]);
+        $results = $stmt->fetchAll();
+    }
+    catch (Exception $e) {
+    }
+
+    // 結果なし
+    if (count($results) === 0) {
+        $return_array['message'] = '※「' + $first + 
+                '」で始まる言葉をもう知りません。私の負けです。'; 
+        return $return_array;
+    }
+
+    $result = $results[0];
+    $return_array['name'] = htmlspecialchars($result['name'], ENT_QUOTES);
+    $return_array['reading'] = $result['reading'];
+    $return_array['last'] = $result['last_clean_long'];
+
+    return $return_array;
+}
